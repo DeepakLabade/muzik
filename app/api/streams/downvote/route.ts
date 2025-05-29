8ab8dd348414adeb54eaff1b66096df5dc6f3409
DeepakLabade <deepaklabade001@gmail.com>
2025-05-29 22:13:31 +0530

    added v1 backend

---
+++ app/api/streams/downvote/route.ts
@@ -0,0 +1,43 @@
+import { prismaClient } from "@/app/lib/db";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+const downvoteSchema = z.object({
+    streamId: z.string()
+})
+
+export async function POST(req: NextRequest) {
+    const session = await getServerSession()
+    const user = await prismaClient.user.findFirst({
+        where: {
+            email: session?.user?.email ?? ""
+        }
+    })
+
+    if(!user) {
+        return NextResponse.json({
+            message: "unauthenticated"
+        }, {
+            status: 403
+        })
+    }
+
+    try {
+    const data = downvoteSchema.parse(await req.json())
+    await prismaClient.upvote.delete({
+        where: {
+            streamId_userId: {
+                userId: user.id,
+                streamId: data.streamId
+            }
+        }
+    })
+    } catch (error) {
+        return NextResponse.json({
+            msg: "error while upvoting"
+        }, {
+            status: 411
+        })
+    }
+}
--- app/api/streams/route.ts
+++ app/api/streams/route.ts
@@ -1,5 +1,9 @@
+import { prismaClient } from "@/app/lib/db";
 import { NextRequest, NextResponse } from "next/server";
+//@ts-ignore
+import youtubesearchapi from "youtube-search-api";
 import { z } from "zod"
+const  YTREG = /^(?:(?:https?:)?\/\/)?(?:www\.)?(?:m\.)?(?:youtu(?:be)?\.com\/(?:v\/|embed\/|watch(?:\/|\?v=))|youtu\.be\/)((?:\w|-){11})(?:\S+)?$/;
 
 const createStreamSchema = z.object({
     creatorId: z.string(),
@@ -8,7 +12,36 @@ const createStreamSchema = z.object({
 
 export async function POST(req: NextRequest) {
     try {
-        const data = createStreamSchema.safeParse(await req.json())
+        const data = createStreamSchema.parse(await req.json())
+        const isYT = data.url.match(YTREG)
+        if(!isYT) {
+            return NextResponse.json({
+                msg: "something went wrong while adding stream",
+            }, {
+                status: 411
+            })
+        }
+        const extractedId = data.url.split("?v=")[1];
+
+        const res = await youtubesearchapi.GetVideoDetails(extractedId)
+        const thumbnails = res.thumbnail.thumbnails
+        thumbnails.sort((a: {width: number}, b: {width: number}) => a.width < b.width ? -1 : 1)
+
+        const stream = await prismaClient.stream.create({
+            data: {
+                userId: data.creatorId,
+                url: data.url,
+                extractedId,
+                type: "Youtube",
+                title: res.title,
+                smallImg: (thumbnails.length > 1 ? thumbnails[thumbnails.length - 2].url : thumbnails[thumbnails.length - 1].url) ?? "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBYYFhUjvNhBJ9SwQTv8X3PyQVtd-EejwOMQ&s",
+                bigImg: thumbnails[thumbnails.length - 1 ].url ?? "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBYYFhUjvNhBJ9SwQTv8X3PyQVtd-EejwOMQ&s"
+            }
+        })
+
+        return NextResponse.json({
+            msg: "stream is added successfully"
+        })
     } catch (error) {
         return NextResponse.json({
             msg: "something went wrong while adding strema",
@@ -17,4 +50,18 @@ export async function POST(req: NextRequest) {
             status: 411
         })
     }
+}
+
+
+export async function GET(req: NextRequest) {
+    const creatorId = req.nextUrl.searchParams.get("creatorId");
+    const streams = await prismaClient.stream.findMany({
+        where: {
+            userId: creatorId ?? ""
+        }
+    })
+
+    return NextResponse.json({
+        streams
+    })
 }
